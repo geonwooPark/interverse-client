87139f3f2c35daaee24d80b8f8e38030a54d7c38
geonwoo <geonwoo@geonwooui-Macmini.local>
2025-05-26 23:03:52 +0900

    Chore: 배포 자동화 설정중

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, useRoutes } from 'react-router-dom'
 import { authRoutes } from './route/auth'
 import { mainRoutes } from './route/main'
 
 export default function Router() {
-  return useRoutes([...authRoutes, ...mainRoutes])
+  return useRoutes([
+    ...authRoutes,
+    ...mainRoutes,
+    { path: '*', element: <Navigate to="/" replace /> },
+  ])
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/',
   resolve: {
     alias: [
       { find: '@components', replacement: '/src/components' },
