416c724c3d32994ef4e9e0ddbefc4d70a033c408
박건우 <white0581@naver.com>
2025-06-01 19:19:28 +0900

    Chore: ConfirmModal props 추가 및 스타일 수정

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -1,19 +1,24 @@
+import { cn } from '@utils/cn'
 import Button from './Button'
 
 interface ConfirmModalProps {
   title: string
   description: string
-  actionLabel?: string
+  leftLabel?: string
+  rightLabel?: string
+  rightButtonClassName?: string
   hasDim?: boolean
   onClose: () => void
-  onSubmit: any
+  onSubmit: () => void
   hideLeftButton?: boolean
 }
 
 function ConfirmModal({
   title,
   description,
-  actionLabel,
+  leftLabel,
+  rightLabel,
+  rightButtonClassName,
   hasDim = true,
   onClose,
   onSubmit,
@@ -26,26 +31,31 @@ function ConfirmModal({
 
       {/* Modal */}
       <div
-        className={`absolute left-[50%] top-[50%] size-full h-fit w-[360px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-white p-4`}
+        className={`absolute left-[50%] top-[50%] size-full h-fit w-[360px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-white`}
       >
-        <div>
-          <p className="body1">{title}</p>
+        <div className="p-4">
+          <p className="text-h6">{title}</p>
         </div>
 
-        <div className="py-6">
-          <p className="whitespace-pre-line text-body2 leading-[1.5]">
+        <div className="mb-4 px-4">
+          <p className="whitespace-pre-line text-body2 leading-normal">
             {description}
           </p>
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex justify-end gap-2 p-4">
           {hideLeftButton || (
             <Button size="md" variant="ghost" onClick={onClose}>
-              취소
+              {leftLabel || '닫기'}
             </Button>
           )}
-          <Button size="md" variant="contained" onClick={onSubmit}>
-            {actionLabel || '확인'}
+          <Button
+            size="md"
+            variant="contained"
+            onClick={onSubmit}
+            className={cn('shadow-none', rightButtonClassName)}
+          >
+            {rightLabel || '확인'}
           </Button>
         </div>
       </div>
