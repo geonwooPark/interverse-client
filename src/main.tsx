7c722a1cf5ff6707ec5031d47b84a20bda8591d7
박건우 <white0581@naver.com>
2025-06-22 15:49:44 +0900

    Chore: dev 브런치 CI/CD 테스트

--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,22 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import ToastProvider from '@providers/ToastProvider/index.tsx'
 import ModalProvider from '@providers/ModalProvider.tsx'
+import React from 'react'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <ToastProvider>
-        <ModalProvider>
-          <App />
-        </ModalProvider>
-      </ToastProvider>
-    </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ToastProvider>
+          <ModalProvider>
+            <App />
+          </ModalProvider>
+        </ToastProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    ,
+  </React.StrictMode>,
 )
