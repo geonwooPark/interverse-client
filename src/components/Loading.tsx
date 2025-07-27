0d953f184de5c4a4aa4e12c109fdba48cbe90228
박건우 <white0581@naver.com>
2025-07-27 15:00:01 +0900

    Style: Loading 스타일 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
+    <div className="fixed inset-0 z-notification flex items-center justify-center font-neodgm text-2xl">
       Loading...
     </div>
   )
