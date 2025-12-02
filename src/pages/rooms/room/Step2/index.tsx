1659bf7fafee1461dc4e448d580489ef9bcdcc0e
geonwooPark <white0581@naver.com>
2025-12-02 21:44:25 +0900

    Feat: 게임 닉네임을 로그인 시 닉네임으로 사용

--- src/pages/rooms/room/Step2/index.tsx
+++ src/pages/rooms/room/Step2/index.tsx
@@ -1,39 +1,32 @@
 import React, { useState } from 'react'
 import AvatarSelector from './AvatarSelector'
-import { MAX_NICKNAME_LENGTH } from '@constants/index'
 import { StepFlowProps } from '@components/StepFlow/types'
 import Button from '@components/Button'
 import GameManager from '@managers/GameManager'
-import { TextFieldWithCaption } from '@components/TextField'
 import Container from '@components/Container'
 import { useParams } from 'react-router-dom'
 import { useJoinRoomMutation } from '@hooks/mutations/roomsMutation'
 import { useCharactersQuery } from '@hooks/queries/assetsQueries'
+import { useMeQuery } from '@hooks/queries/authQueries'
 import { useTranslation } from 'react-i18next'
 
 interface Step2Props extends Partial<StepFlowProps> {}
 
 // 방 입장
-export default function Step2({ activeStep, onNext }: Step2Props) {
+export default function Step2({ onNext }: Step2Props) {
   const { id: roomId } = useParams()
 
+  const { t } = useTranslation()
+
   const { data: characters } = useCharactersQuery()
+
+  const { data: me } = useMeQuery()
 
   const { mutate } = useJoinRoomMutation()
 
   const [texture, setTexture] = useState(0)
 
-  const [nickname, setNickname] = useState('')
-
-  const { t } = useTranslation()
-
-  const onNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value.length > MAX_NICKNAME_LENGTH) {
-      e.target.value = e.target.value.slice(0, MAX_NICKNAME_LENGTH)
-    }
-
-    setNickname(e.target.value)
-  }
+  const nickname = me?.user?.nickname
 
   const onTextureChange = (value: number) => {
     setTexture(value)
@@ -62,27 +55,11 @@ export default function Step2({ activeStep, onNext }: Step2Props) {
       <Container className="max-w-[360px]">
         <AvatarSelector texture={texture} onChange={onTextureChange} />
 
-        <div className="mb-6 mt-3">
-          <TextFieldWithCaption
-            value={nickname}
-            onChange={onNicknameChange}
-            placeholder={t('rooms.room.step2.nickname_placeholder')}
-            maxLength={MAX_NICKNAME_LENGTH}
-            caption={
-              <p className="ml-2 mt-1 text-caption text-gray-700">
-                {t('rooms.room.step2.nickname_caption', {
-                  length: MAX_NICKNAME_LENGTH,
-                })}
-              </p>
-            }
-          />
-        </div>
-
         <Button
           size="md"
           variant="contained"
           fullWidth
-          disabled={nickname.length === 0}
+          disabled={!me?.user?.nickname}
           onClick={onEnter}
         >
           {t('rooms.room.step2.enter')}
